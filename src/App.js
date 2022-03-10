7526887d26c956e4719a60ffa3a5bdc985369ea3
shruthi093 <jayasruthi.l@gmail.com>
2022-03-10 11:23:01 +0530

    10.03 protected routes

--- src/App.js
+++ src/App.js
@@ -12,48 +12,116 @@ import ContextComponent from "./Components/ContextComponent";
 import {ContextUpdate} from "./Components/UpdateContext"
 import { Parent } from "./Components/FunctionalComponent";
 import {User} from "./Components/RefComponentTwo"
-import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom"
+import {BrowserRouter as Router,Routes,Route,Link, Navigate} from "react-router-dom"
 import Layout from "./Components/Layout";
-import {Home,DashBoard,About} from "./Components/Layout"
+//import {Home,DashBoard,About} from "./Components/Layout"
 import LoginComponent from "./Components/LoginComponent"
-{/*const handleAPI=async (text)=>{
-    console.log(text)
-   const res=await axios.get("https://hn.algolia.com/api/v1/search?query="+text)
-   return res.data.hits
-}
-<TestComponent msg="final" count={0}></TestComponent>
-<Parent></Parent>
-<User></User>*/}
+import { useState } from "react";
+
 const App=()=>{
+    const [user,setUser]=useState(null)
+    const handleSignIn=()=>setUser({username:"john",email:"john@gmail.com"})
+    const handleSignOut=()=>setUser(null)
+
     return(
         <div>
-            <h1>React Hooks</h1>
-        <p>Accepts a function that contains imperative, possibly effectful code.
-Mutations, subscriptions, timers, logging, and other side effects are not allowed inside the main body of a function component (referred to as React’s render phase). Doing so will lead to confusing bugs and inconsistencies in the UI.
-Instead, use useEffect. The function passed to useEffect will run after the render is committed to the screen. Think of effects as an escape hatch from React’s purely functional world into the imperative world.
-By default, effects run after every completed render, but you can choose to fire them only when certain values have changed.
-    </p>
-<Router>
+            
+{/**<Router>
     <button>
     <Link to="/login"> Login</Link>  
     </button>
     <Routes>
     
-        {/** nested routes */}
+      
     <Route path="/" element={<Layout></Layout>}>
         <Route path="home" element={<Home></Home>}></Route>
         <Route path="dashboard/:username/:role" element={<DashBoard></DashBoard>}></Route>
         <Route path="about" element={<About></About>}></Route>
     </Route>
-    {/** navigating */}
+   
     <Route path="/login" element={<LoginComponent></LoginComponent>}></Route>
     </Routes>
-</Router>
-
-
-
-
+    </Router>*
+    //protected Routes*/}
+    <Router>
+        <Navigation></Navigation>
+{
+    user?
+    
+    <button onClick={handleSignOut}>Sign Out</button>:
+    <button onClick={handleSignIn}>Sign In</button>
+}
+    <Routes>
+        <Route path="/home" element={
+        <ProtectedRoute user={user}>
+        <Home>
+        </Home>
+        </ProtectedRoute>
+        }></Route>
+         <Route path="/dashboard" element={
+        <ProtectedRoute user={user}>
+        <DashBoard></DashBoard>
+        </ProtectedRoute>
+        }></Route>
+        <Route path="/landing" element={<Landing></Landing>}></Route>
+    </Routes>
+    </Router>
         </div>
     )
 }
+const Landing=()=>{
+    return(
+        <div>
+            <h1>Landing Page : Public</h1>
+        </div>
+    )
+}
+const Home=(props)=>{
+    return(
+        <div>
+        <h1>Home Page : Private</h1>
+        </div>
+    )
+}
+const DashBoard=(props)=>{
+    return(
+        <div>
+        <h1>DashBoard  : Private</h1>
+        </div>
+    )
+}
+/*const Home =(props)=>{
+    if(props.user){
+        return(
+            <div>
+                 <h1>Home Page : Private</h1>
+            </div>
+        )
+    }
+    return(
+     /*   <div>
+             <h1>Sorry Login again!</h1>
+        </div>
+        <Navigate to="/landing"></Navigate>
+
+    )   
+}*/
+const Navigation=()=>{
+    return(
+    <nav>
+        <Link to="/home">Home</Link> <br></br>
+        <Link to="/landing">landing</Link><br></br>
+        <Link to="/dashboard">dashboard</Link>
+    </nav>
+    )
+}
+const ProtectedRoute =(props)=>{
+  if(props.user){
+    return props.children
+  }
+  else{
+      return <Navigate to="/landing"></Navigate> 
+  }
+}
+
 export default App;
